1f7c45d7a30ce07c0eae3ad580996b4f1a688d44
Saju Mulakkal Joseph <sajumulakkal@hotmail.com>
2022-02-26 19:27:09 +0800

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,126 @@
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const currentTask = process.env.npm_lifecycle_event
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const fse = require('fs-extra')
 
-var srcFolder = 'src';
-var distFolder = 'dist';
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist/'),
-        filename: 'bundle.js'
-    },
+const postCSSPlugins = [
+  require('postcss-import'),
+  require('postcss-mixins'),
+  require('postcss-simple-vars'),
+  require('postcss-nested'),
+  // If in the future the creator of the postcss-hexrgba package
+  // releases an update (it is version 2.0.1 as I'm writing this)
+  // then it will likely work with PostCSS V8 so you can uncomment
+  // the line below and also install the package with npm.
+  //require('postcss-hexrgba'),
+  require('autoprefixer')
+]
+
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy images', function() {
+      fse.copySync('./app/assets/images', './docs/assets/images')
+    })
+  }
+}
+
+let cssConfig = {
+  test: /\.css$/i,
+  use: ['css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
+}
+
+let pages = fse.readdirSync('./app').filter(function(file) {
+  return file.endsWith('.html')
+}).map(function(page) {
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`
+  })
+})
+
+let config = {
+  entry: './app/index.js',
+  plugins: pages,
+  module: {
+    rules: [
+      cssConfig
+    ]
+  },
+
     externals: {
-        express: 'express',
+          express: 'express',
       },
-    resolve: {
-        extensions: ['.js', '.jsx'],
+  resolve: {
+        extensions: ['.ejs','.js', '.jsx'],
         fallback: {
              "crypto": false,
-	     "https": false,
-	     "url": false,	
-	     "querystring": false,
-	     "zlib": false,
-	     "util": false,
-	     "console": false,
-	     "path": false 
-            },
-            alias: {
-                'ejs': 'ejs.min.js'
-              }
-      },
-    module: {
-        rules: [
-            {
-                test: /.(js,ejs)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-
-            },{
-                test: /.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },{
-                test: /.(png,svg,jpg,gif)$/,
-                use: [`file-loader`]
+             "zlib": false,
+             "util": false,
+             "console": false,
+             "crypto": false,
+             "http": false,
+             "os": false,
+             "fs": false,
+             "dns": false,
+             "net": false,
+             "tls": false,
+             "stream": false,
+             "path": false,
+             "https": false
             }
-        ]
+      },
+}
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  }
+  config.devServer = {
+    before: function(app, server) {
+      server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  }
+  config.mode = 'development'
+}
+
+if (currentTask == 'build') {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
     }
-};
+  })
+
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'docs')
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: {chunks: 'all'},
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()]
+  }
+  config.plugins.push(
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+    //new RunAfterCompile()
+  )
+}
+
+module.exports = config
